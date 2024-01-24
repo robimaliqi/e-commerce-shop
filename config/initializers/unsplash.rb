2c7a3c31f8917ecd293db12e7ea2fa10b4062cbb
Robi <robi.maliqi@hotmail.co.uk>
2024-01-24 22:29:13 +0000

    Add configuration file for unsplash

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,5 @@
+Unsplash.configure do |config|
+  config.application_access_key = 'XbbrdvZoWLfL87jWQ34E1O4VvwF5UouaPw2RAU6gwl0'
+  config.application_secret = 'gWS8hvA4rKuIrJsEXsjaECTmVtrdcuk1n3U3ZoBAw4w'
+  config.utm_source = 'e-commerce-shop'
+end
