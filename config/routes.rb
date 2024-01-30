b83cc076cbe520eb124c2b3c71cf78220e34164a
Robi <robi.maliqi@hotmail.co.uk>
2024-01-30 08:16:26 +0000

    Update routes for new carts model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [:index, :show]
   root 'home#index'
+
+  namespace :carts do
+    resource :add, only: :create
+  end
 end
