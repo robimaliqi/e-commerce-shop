96d95f7da396be5e07f90b68de2711180cd5c55a
Robi <robi.maliqi@hotmail.co.uk>
2024-01-30 09:41:27 +0000

    Add user_id column to cart table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_225134) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_093822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_225134) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["token"], name: "index_carts_on_token", unique: true
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_225134) do
 
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
+  add_foreign_key "carts", "users"
 end
