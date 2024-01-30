876335c859146e8e82dd722cc93993672a0c4813
Robi <robi.maliqi@hotmail.co.uk>
2024-01-30 09:00:08 +0000

    Added current_cart method to application controller to make sure user is signed in to be able to purchase items in the cart but if not signed in can still add and track items in the current using cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def current_cart
+    cart = Cart.find_or_create_by(user: current_user)
+    cookies[:cart_token] ||= cart.token
+    cart
+  end
+  helper_method :current_cart
 end
