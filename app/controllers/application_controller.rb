9df28e298f9f5549bc49e0775a9d5b4f9828856d
Robi <robi.maliqi@hotmail.co.uk>
2024-01-31 07:36:47 +0000

    Changed the current_cart method in Application controller so that it allows a guest user and a signed up user to add items to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
 
   def current_cart
-    cart = Cart.find_or_create_by(user: current_user)
+    if user_signed_in?
+      cart = Cart.find_or_create_by(user: current_user)
+    else
+      cart = Cart.find_or_create_by(token: cookies[:cart_token])
+    end
+  
     cookies[:cart_token] ||= cart.token
     cart
   end
