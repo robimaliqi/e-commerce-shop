7773868cc5ee11cc71eeedfe20f5a86431a45e06
Robi <robi.maliqi@hotmail.co.uk>
2024-02-01 21:22:06 +0000

    Add post route for search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [:index, :show]
   resource :checkout, only: :show
+  post :search, to: "searches#show"
   root 'home#index'
 
   namespace :carts do
