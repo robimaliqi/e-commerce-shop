e91321eedf9f3def877c1855a1e3bdd10b5305e4
Robi <robi.maliqi@hotmail.co.uk>
2024-02-01 10:48:05 +0000

    Add logic to CartItem model to update cart item counter wihtout refreshing the page

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,18 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  after_create_commit do
+    broadcast_replace_to cart,
+                         target: "cart_count",
+                         partial: "cart/item_count",
+                         locals: { count: cart.quantity }
+  end
+
+  after_update_commit do
+    broadcast_replace_to cart,
+                         target: "cart_count",
+                         partial: "cart/item_count",
+                         locals: { count: cart.quantity }
+  end
 end
