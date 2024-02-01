f55e42eeb7bb439d7a0a9d859d633d2644b6fdb2
Robi <robi.maliqi@hotmail.co.uk>
2024-02-01 19:49:10 +0000

    Add total price logic in cart_items models

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -9,6 +9,10 @@ class CartItem < ApplicationRecord
                          target: "cart_count",
                          partial: "cart/item_count",
                          locals: { count: cart.quantity }
+    broadcast_replace_to cart,
+                         target: "total_price",
+                         partial: "cart/total_price",
+                         locals: { current_cart: cart }
   end
 
   after_update_commit do
@@ -21,6 +25,10 @@ class CartItem < ApplicationRecord
                          target: dom_id(self, "quantity"),
                          partial: "cart/item_quantity",
                          locals: { cart_item: self }
+    broadcast_replace_to cart,
+                         target: "total_price",
+                         partial: "cart/total_price",
+                         locals: { current_cart: cart }
   end
 
   after_destroy_commit do
@@ -29,5 +37,13 @@ class CartItem < ApplicationRecord
                          target: "cart_count",
                          partial: "cart/item_count",
                          locals: { count: cart.quantity }
+  broadcast_replace_to cart,
+                         target: "total_price",
+                         partial: "cart/total_price",
+                         locals: { current_cart: cart }
+  end
+
+  def total_price
+    quantity.to_i * product.price.to_f
   end
 end
