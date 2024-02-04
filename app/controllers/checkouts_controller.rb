68f3a0cbed9dd37d62cb546eb64cd5eb88beaf51
Robi <robi.maliqi@hotmail.co.uk>
2024-02-04 11:16:07 +0000

    Add autheticate user, create and cart items path to checkouts controller to work with stripe checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,4 +1,37 @@
 class CheckoutsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+  end
+
+  def create
+    session = Stripe::Checout::Session.create(
+      billing_address_collection: :auto,
+      mode: :payment,
+      payment_method_type: ['card'],
+      line_items: ,
+      success_url: checkout_payment_url,
+      cancel_url: root_url
+    )
+
+    current_cart.update!(stripe_session_id: session.id)
+    redirect_to session.url, allow_other_host: true
+  end
+
+  private
+
+  def cart_items
+    current_cart.cart_items.map do |cart_item|
+      {
+        price_data: {
+          currency: "gbp",
+          product_data: {
+            name: cart_item.product.name
+          },
+          unit_amount: cart_item.product.price.to_i * 100
+        },
+        quantity: cart_item.quantity
+      }
+    end 
   end
 end
