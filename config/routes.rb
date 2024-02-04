efa44628a952b8dd63e1a087e87b944dc2142774
Robi <robi.maliqi@hotmail.co.uk>
2024-02-04 11:15:02 +0000

    Update routes to add payment path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [:index, :show]
-  resource :checkout, only: :show
   post :search, to: "searches#show"
   get :search, to: "searches#show"
   root 'home#index'
+
+  resource :checkout, only: [:show, :create] do
+    resource :payment, only: :show
+  end
 
   namespace :carts do
     resource :add, only: :create
