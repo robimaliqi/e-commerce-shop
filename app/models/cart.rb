28ee1c896069ea2ee2171e14ad9aeb979fe8e3e6
Robi <robi.maliqi@hotmail.co.uk>
2024-02-04 11:19:22 +0000

    Added enum to cart models for different status scenario's

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,14 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
 
+  enum status: {
+    checkout_pending: 0,
+    payment_pending: 1,
+    payment_complete: 2,
+    fullfilled: 3,
+    cancelled: 4
+  }
+
   def quantity
     cart_items.sum(&:quantity)
   end
