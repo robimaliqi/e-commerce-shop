2d706be8b2c8dbc5653e130fe3bb2d9c961a1c6b
Robi <robi.maliqi@hotmail.co.uk>
2024-02-05 08:33:31 +0000

    Added a destroy cart and cart session for logged in user in Payments controller in the show method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
   def show
-    current_cart&.payment_pending! unless current_cart&.checkout_pending?
+    current_cart&.payment_pending!
+    current_user.cart.cart_items.destroy_all if current_user&.cart
     cookies.delete(:cart_token)
   end
 end
