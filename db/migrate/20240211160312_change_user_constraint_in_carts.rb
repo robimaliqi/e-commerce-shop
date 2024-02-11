6cc3109591d7b835097d39f8e500a5132cc42cf5
Robi <robi.maliqi@hotmail.co.uk>
2024-02-11 16:14:15 +0000

    Removed null:false in the User database column so that user can null

---
+++ db/migrate/20240211160312_change_user_constraint_in_carts.rb
@@ -0,0 +1,5 @@
+class ChangeUserConstraintInCarts < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :carts, :user_id, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_04_110224) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_160312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_04_110224) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.integer "status", default: 0
     t.string "stripe_session_id"
     t.index ["token"], name: "index_carts_on_token", unique: true
