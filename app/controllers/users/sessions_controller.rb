c1dfbd2b5a2677c891a9c7cc0ef4bdcca19f4877
Robi <robi.maliqi@hotmail.co.uk>
2024-02-20 21:52:27 +0000

    Generated sessions controller to help display error message if the log in details are invalid

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+    else
+      flash.now[:alert] = "Invalid Email or password"
+      render :new
+    end
+  end
+end
