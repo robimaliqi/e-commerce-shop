34ae9b331b6ad3fd8e517cec68a012ab64685b91
Robi <robi.maliqi@hotmail.co.uk>
2024-04-02 19:54:13 +0100

    Put SessionsContorller class into module Users, to fix bug with Zeitwerk gem as it was not finding Users within the fiile

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,12 @@
-class SessionsController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-    else
-      flash.now[:alert] = "Invalid Email or password"
-      render :new
+module Users 
+  class SessionsController < ApplicationController
+    def create
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+      else
+        flash.now[:alert] = "Invalid Email or password"
+        render :new
+      end
     end
   end
 end
